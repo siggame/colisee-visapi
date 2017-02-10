4f385f106a78c9461127624525a51b5fa7e8cf9e
Matthew Qualls <mqggd@mst.edu>
2017-02-10 09:48:19 -0600

    Todo: Add routing to main.ts
    Added (204: Gamelog Not Found) handling to getGamelog()
    
    changed expres.ts to main.ts, removed app.ts and the original main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
-import { App } from "./app";
+import * as visapi from "./visapi"
 
-let app = new App();
+var express = require('express')
+var app = express()
 
-app.start();
+var getGamelog = function (req, res) {
+  visapi.getGamefile()
+    .then((gameObjects)=>{
+    	if(gameObjects == null) {
+        return res.status(204).send('Gamelog Not Found');
+      }
+      return gameObjects.gamelog;
+    })
+    .catch(err=>{
+    	res.status(400)
+    });
+}
 
-
+app.get('/', getGamelog)
