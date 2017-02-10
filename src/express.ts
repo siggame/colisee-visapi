f04539464e603068df8b584fc66f484bde090c49
Matthew Qualls <mqggd@mst.edu>
2017-02-09 22:08:24 -0600

    getGamelog is a thing now
    getGamelog function in express.ts gets a {id, gamelog} from getGamefile
    in visapi.ts and returns the gamelog
    added a little app.get which calls it
    
    Need to added something to give a 204 error when no gamelog exists

--- src/express.ts
+++ src/express.ts
@@ -1,9 +1,19 @@
 import * as visapi from "./visapi"
+
 var express = require('express')
 var app = express()
 
-var getGamefile = function (req, res) {
-  visapi.getGamelog(10);
+var getGamelog = function (req, res) {
+  visapi.getGamefile()
+    .then((id)=>{
+    	visapi.markVisualized(id)
+    })
+    .then((gamelog)=>{
+    	res.send(gamelog)
+    })
+    .catch(err=>{
+    	res.status(400)
+    })
 }
 
-app.use(getGamefile)
+app.get('/', getGamelog)
--- src/visapi.ts
+++ src/visapi.ts
@@ -1,16 +1,48 @@
 import * as db from "./dbUtil"
 
+/** @module visapi */
 
-export function getGamelog(num: number): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-    	db.query('game').where('visualized', false)
-    	  .orderBy('modified_time').desc()
-    	  .limit(num)
-    	  .then()
+export function getGamefile(): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      db.select('id', 'gamelog').from('game')
+        .where('visualized', false)
+        .orderBy('modified_time').desc()
+        .limit(1)
+        .then(resolve)
+        .catch(reject);
     })
 }
 
+export function markVisualized(gameNum: number) {
+  db.query('game').where('Id', gameNum)
+    .update('visualized', true)
+}
+
+
 /*
+export function getNNewGamelogs(num: number): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+    	db.query('game').where('visualized', false)
+    	  .orderBy('modified_time').desc()
+    	  .limit(num)
+        .then(resolve)
+        .catch(reject);
+    })
+}
+
+export function getNOldGamelogs(num: number): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      db.query('game').where('visualized', false)
+        .orderBy('modified_time').asc()
+        .limit(num)
+        .then(resolve)
+        .catch(reject);
+    })
+}
+*/
+
+
+/**
 export function checkPrioriy(gameNum: number): Promise<void> {
 	return new Promise<void>((resolve, reject) => {
 		db.query('Priority').where('Serial', gameNum)
