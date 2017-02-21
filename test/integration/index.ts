0b0982dd283d9d0d5595632190010a4c2f14e546
user404d <conduffql@gmail.com>
2017-02-21 17:53:59 -0600

    updating db connection test

--- test/integration/index.ts
+++ test/integration/index.ts
@@ -1,17 +1,18 @@
-import * as db from "../../src/db";
+import * as chai from "chai"
+import * as db from "../../src/db"
 import visapiTest from "./visapi"
 
 
-export default function() : void {
-  describe("Integration Tests", function(){
-    it("Should connect to the database", function(){
+export default function (): void {
+  describe("Integration Tests", function () {
+    it("Should connect to the database", function () {
       this.retries(5);
-      return new Promise((resolve, reject)=>{
+      return new Promise((resolve, reject) => {
         db.query.raw("SELECT 1+1")
           .then(resolve)
-          .catch(reject);
+          .catch(err => chai.expect(err).to.not.exist)
       })
     })
-    visapiTest();
   })
+  visapiTest()
 }
